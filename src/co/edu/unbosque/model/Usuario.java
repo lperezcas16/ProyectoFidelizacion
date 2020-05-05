92fd68ede4b79bf7e3d450c31e35b0dffa8d185e
Tatiana Pulido <Tatiana Pulido@192.168.0.6>
2020-05-04 23:49:29 -0500

    Creacion de clase en Paquete modelo y Paquete modelo.persistence

--- src/co/edu/unbosque/model/Usuario.java
+++ src/co/edu/unbosque/model/Usuario.java
@@ -1,5 +1,122 @@
 package co.edu.unbosque.model;
 
-public class Usuario {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Usuario implements Serializable {
+
+	private String numeroCedula;
+	private String nombre;
+	private String genero;
+	private String correo;
+	private String numeroTarjeta;
+	private long cupoTargeta;
+	private ArrayList<String> parejas;
+	private String usuario;
+	private String contraseña;
+	private String tipo;
+
+	public Usuario(String numerocedula, String nombre, String genero,
+			String correo, String numeroTarjeta, long cupoTarjeta,
+			ArrayList<String> parejas, String usuario, String contraseña) {
+		this.numeroCedula = numerocedula;
+		this.nombre = nombre;
+		this.genero = genero;
+		this.correo = correo;
+		this.numeroTarjeta = numeroTarjeta;
+		this.cupoTargeta = cupoTarjeta;
+		this.usuario = usuario;
+		this.contraseña = contraseña;
+		parejas = new ArrayList<String>();
+		this.parejas = parejas;
+	}
+
+	public Usuario(String nombre, String correo, String usuario,
+			String contraseña) {
+		this.nombre = nombre;
+		this.correo = correo;
+		this.usuario = usuario;
+		this.contraseña = contraseña;
+	}
+
+	public String getNumeroCedula() {
+		return numeroCedula;
+	}
+
+	public void setNumeroCedula(String numeroCedula) {
+		this.numeroCedula = numeroCedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getNumeroTarjeta() {
+		return numeroTarjeta;
+	}
+
+	public void setNumeroTarjeta(String numeroTarjeta) {
+		this.numeroTarjeta = numeroTarjeta;
+	}
+
+	public long getCupoTargeta() {
+		return cupoTargeta;
+	}
+
+	public void setCupoTargeta(long cupoTargeta) {
+		this.cupoTargeta = cupoTargeta;
+	}
+
+	public ArrayList<String> getParejas() {
+		return parejas;
+	}
+
+	public void setParejas(ArrayList<String> parejas) {
+		this.parejas = parejas;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
 }
