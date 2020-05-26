65a59fe1637de1c9e95f1d49dd11ee903d75d6e9
Tatiana Pulido <Tatiana Pulido@192.168.0.2>
2020-05-26 13:56:58 -0500

    Paquete test

---
+++ test/UsuariosDAOTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+public class UsuariosDAOTest extends TestCase {
+
+	File file = new File(".\\dataTest\\baseDatosTest.out");
+
+}
